08850cf39e134cb2291443cf7fe137b53921fdb4
FukuiReTu <fukui_retu@FukuiReTu-no-MacBook-Air.local>
2014-05-01 00:39:14 +0900

    modify

--- lib/fluent/plugin/out_mysql_load.rb
+++ lib/fluent/plugin/out_mysql_load.rb
@@ -1,2 +1,35 @@
 class Fluent::MysqlLoadOutput < Fluent::BufferedOutput
+  Fluent::Plugin.register_output('mysql_load', self)
+  
+  def initialize
+    require 'mysql2'
+    super
+  end
+
+  def configure(conf)
+    super
+    # @path = conf['path']
+  end
+
+  def start
+    super
+    # init
+  end
+
+  def shutdown
+    super
+    # destroy
+  end
+
+  def format(tag, time, record)
+    [tag, time, record].to_msgpack
+  end
+
+  def write(chunk)
+    records = []
+    chunk.msgpack_each { |record|
+      # records << record
+    }
+    # write records
+  end
 end
