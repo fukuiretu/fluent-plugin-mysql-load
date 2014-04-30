50345fddda12144b7bf8203de0d8cfbb8640faf2
FukuiReTu <fukui_retu@FukuiReTu-no-MacBook-Air.local>
2014-05-01 01:04:29 +0900

    modify

--- lib/fluent/plugin/out_mysql_load.rb
+++ lib/fluent/plugin/out_mysql_load.rb
@@ -1,24 +1,32 @@
 class Fluent::MysqlLoadOutput < Fluent::BufferedOutput
   Fluent::Plugin.register_output('mysql_load', self)
-  
+
   def initialize
     require 'mysql2'
     super
   end
 
+  config_param :host, :string, :default => 'localhost'
+  config_param :port, :integer, :default => 3306
+  config_param :username, :string, :default => 'root'
+  config_param :password, :string, :default => nil
+  config_param :database, :string, :default => nil
+  config_param :encoding, :string, :default => 'utf8'
+  config_param :columns, string, :default => nil
+
   def configure(conf)
     super
-    # @path = conf['path']
+    if password.nil? || database.nil?
+      raise Fluent::ConfigError, "password and  database is required!"
+    end
   end
 
   def start
     super
-    # init
   end
 
   def shutdown
     super
-    # destroy
   end
 
   def format(tag, time, record)
@@ -26,10 +34,30 @@ class Fluent::MysqlLoadOutput < Fluent::BufferedOutput
   end
 
   def write(chunk)
+    # tmpファイルにvalueをTSVで出力する
+    # keyはカンマ区切りの文字列にする
+    # ただしcolumnsが指定されている場合、それに従う
     records = []
     chunk.msgpack_each { |record|
-      # records << record
     }
-    # write records
+
+    # load data infile のクエリを生成する
+    query = ''
+    # クエリを実行する
+    connection = self.connection
+    connection.query(query)
+    connection.close
+  end
+
+  private
+  def connection
+    Mysql2::Client.new({
+        :host => @host, 
+        :port => @port,
+        :username => @username, 
+        :password => @password, 
+        :database => @database,
+        :encoding => @encoding
+      })
   end
 end
